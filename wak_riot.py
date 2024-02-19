1eb166f146507f08e7200897fc7848f41df32e0c
liveandletlive <86241587+KGochae@users.noreply.github.com>
2024-02-19 16:55:41 +0900

    Update wak_riot.py
    img 주소 변경

--- wak_riot.py
+++ wak_riot.py
@@ -479,7 +479,7 @@ def death_spot(df):
 
     # 이미지로 스캐터 플롯 생성
     for x, y in zip(x_data, y_data):
-        url = 'C:/test/img/death.png'
+        url = 'img/death.png'
         img = Image.open(url)
 
         # 원형 이미지로 스캐터 플롯 생성
@@ -506,7 +506,7 @@ def death_spot_sc(df,color):
     plt.axis('off')
 
     # 지도 이미지 추가
-    map_path = 'C:/test/black_map.jpg'
+    map_path = 'img/black_map.jpg'
     img = Image.open(map_path)
     ax.imshow(img, extent=[0, 15000, 0, 15000])
 
